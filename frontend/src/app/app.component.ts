edf218afd01ca24b6db108c246eef68db94d41b4
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-10-16 14:32:22 +0200

    Use chart component to render chart

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
 declare const Dygraph;
 declare const socketio;
+
+const temp = [
+    [new Date(1504869255000), 23.1,],
+    [new Date(1504868952000), 23.1,],
+    [new Date(1504868649000), 23.1,],
+    [new Date(1504868347000), 23.1,],
+    [new Date(1504868044000), 23.2,],
+    [new Date(1504867741000), 23.2,],
+    [new Date(1504867438000), 23.2,],
+    [new Date(1504867136000), 23.2,],
+    [new Date(1504866833000), 23.2,],
+    [new Date(1504866530000), 23.2,],
+    [new Date(1504866228000), 23.2,],
+    [new Date(1504865925000), 23.3,],
+    [new Date(1504865622000), 23.3,],
+    [new Date(1504865319000), 23.3,],
+    [new Date(1504865017000), 23.3,],
+    [new Date(1504864714000), 23.3,],
+    [new Date(1504864411000), 23.4,],
+    [new Date(1504864108000), 23.4,],
+    [new Date(1504863806000), 23.4,],
+    [new Date(1504863503000), 23.4,]
+];
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>
-      Welcome to {{title}}!!
+      Weather Station
     </h1>
 
-    <div id="chart"></div>
+    <ex-graph
+        [labels]="['Temperature', 'Value']"
+        [data]="tempData"
+        [loading]="tempReady"
+        range="null"
+        topTitle="Temperature"
+    ></ex-graph>
+
   `,
   styles: []
 })
 export class AppComponent implements OnInit {
-    title = 'app';
+    tempData : Object = {'data' : [], 'labels' : []};
+    tempReady = true;
 
     g = null;
 
     ngOnInit() {
-        console.log("hello");
-        this.g = new Dygraph("chart", [[1,2], [2,1]])
+        this.setData();
+    }
+
+    public setData() {
+        this.tempData['data'] = temp;
+        this.tempData['labels'] = ['Temperature', 'Value']
+        this.tempReady = false;
     }
 }
