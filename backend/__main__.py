783ca6878072a8fc21aa2656b1946cac840c7b61
Petr Stehlik <petrstehlik@raspberrypi>
2017-11-04 11:30:09 +0000

    Add light and moisture to holder and database

--- backend/__main__.py
+++ backend/__main__.py
@@ -7,10 +7,12 @@ from WebSocket import getAll, app, publish_data, sio
 MQTT_TEMP = "home/ws/sensor/temperature"
 MQTT_HUM = "home/ws/sensor/humidity"
 MQTT_PRES = "home/ws/sensor/pressure"
+MQTT_LIGHT = "home/ws/sensor/light"
+MQTT_MOISTURE = "home/ws/sensor/moisture"
 HOST = "10.0.0.32"
 PORT = 1883
 
-conn = sqlite3.connect('test.sq3', check_same_thread=False)
+conn = sqlite3.connect('db.sq3', check_same_thread=False)
 
 def store_record(timestamp, data):
     log = logging.getLogger("STORE")
@@ -20,10 +22,10 @@ def store_record(timestamp, data):
     log.info("Should store data")
     print(data)
 
-    statement = "INSERT INTO {} (time, temperature, pressure, humidity) VALUES ("\
-            "?, ?, ?, ?)".format("weather_data")
+    statement = "INSERT INTO {} (time, temperature, pressure, humidity, light, moisture) VALUES ("\
+            "?, ?, ?, ?, ?, ?)".format("weather_data")
 
-    c.execute(statement, (timestamp, data["temperature"], data["pressure"], data["humidity"]))
+    c.execute(statement, (timestamp, data["temperature"], data["pressure"], data["humidity"], data["light"], data["moisture"]))
 
     conn.commit()
     c.close()
@@ -39,11 +41,13 @@ if __name__ == "__main__":
             "time INTEGER PRIMARY KEY, "\
             "temperature REAL NOT NULL,"\
             "pressure REAL NOT NULL,"\
+            "light REAL NOT NULL,"\
+            "moisture REAL NOT NULL,"\
             "humidity REAL NOT NULL);")
     conn.commit()
     c.close()
 
-    holder = Holder(HOST, PORT, [MQTT_TEMP, MQTT_HUM, MQTT_PRES])
+    holder = Holder(HOST, PORT, [MQTT_TEMP, MQTT_HUM, MQTT_PRES, MQTT_LIGHT, MQTT_MOISTURE])
     holder.on_end = store_record
 
     #web.run_app(app, host='127.0.0.1', port=8080)
