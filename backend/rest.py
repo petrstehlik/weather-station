f7fcd9424f7808e160328df84902662cc291e00f
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-11-08 10:10:22 +0100

    Add weather forecast

--- backend/rest.py
+++ backend/rest.py
@@ -3,6 +3,7 @@ import logging
 import sqlite3
 import json
 import time
+import requests
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
@@ -12,10 +13,12 @@ app = Flask(__name__)
 conn = sqlite3.connect('db.sq3', check_same_thread=False)
 conn.row_factory = sqlite3.Row
 
+APPID = "c11eabf697cfe8167bb53d3c1a7cbd20"
+
 @app.route('/init')
 def init():
     c = conn.cursor()
-    c.execute("SELECT * FROM weather_data WHERE time >= {}".format(int(time.time()) - (60 * 60 * 24)))
+    c.execute("SELECT * FROM weather_data WHERE time >= {}".format(int(time.time()) - (60 * 60 * 2)))
 
     res = c.fetchall()
 
@@ -35,6 +38,9 @@ def init():
 
             elif i == "moisture":
                 val = (1023 - val) / 10.23
+
+            elif i == "pressure":
+                val = val / 100.0
 
             result[i].append([item['time'] * 1000, val])
 
@@ -57,6 +63,19 @@ def latest():
         elif i == "moisture":
             val = (1023 - val) / 10.23
 
+        elif i == "pressure":
+                val = val / 100.0
+
+
         result[i] = [res[0]['time'] * 1000, val]
 
     return(json.dumps(result))
+
+@app.route("/weather")
+def weather():
+    r = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=3078610&units=metric&appid={}".format(APPID))
+
+    if r.status_code == 200:
+        return(json.dumps(r.json()))
+    else:
+        print(str(r))
